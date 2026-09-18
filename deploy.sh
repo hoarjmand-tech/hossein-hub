8bf8f28c82b103dd89bf941a764c99b730baa800
hoarjmand-tech <ho.arjmand@gmail.com>
2026-09-18 18:21:32 +0330

    Make deploy fast and deployment-only

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 cd /opt/hossein-hub
-exec bash bootstrap-all.sh
+git fetch origin main
+git reset --hard origin/main
+docker compose -f docker-compose.yml up -d --build --remove-orphans
+echo "DEPLOY OK"
